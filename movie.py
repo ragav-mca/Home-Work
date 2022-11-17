6492126a1b336465f87ad9fce5c45da7a7ee7248
ragav-mca <110483081+ragav-mca@users.noreply.github.com>
2022-11-18 01:32:59 +0530

    Python Strings
    Python Strings

--- movie.py
+++ movie.py
@@ -1,23 +1,7 @@
-# Ask your friend all the movies s/he watched recently. List all the movies that you both have watched,
-# only the movies you watched, and only the movies your friend  watched.
+#Get a movie name. Find out how many words are in that movie. 
 
-s1 = input("Enter the movies watched by the friend recently: ")
-s2 = input("Enter all the movies that you both watched: ")
-s3 = input("Enter only the movies watched by you: ")
-s4 = input("Enter only the movies your friend watched: ")
-commonChars = ""
-errmsg = "No same movie has been watched by both"
+movie_name = input("Enter the movie name: ")
+print("The movie name is: ", movie_name)
 
-for i in range(0,len(s1)):
-    s1char = s1[i]
-    for j in range(0,len(s2)):
-        s2char = s2[j]
-        if s1char == s2char:
-            commonChars += s2char
-print(s3)
-print(s4)
-print(commonChars)
-if(len(commonChars) == 0):
-    print(errmsg)
-
-
+count_words = movie_name.count(" ") + 1
+print("The number of word in the movie are: ", str(count_words))
